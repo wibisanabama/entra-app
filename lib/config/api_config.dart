27d66845ab7f47e3d892ef75ac2f0f3422abab11
wibisanabama <wibisanabama@gmail.com>
2026-08-11 08:26:45 +0700

    chore(config): update host IP to 192.168.0.106 for physical device connection

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,7 +1,7 @@
 class ApiConfig {
   // Use 10.0.2.2 for Android emulator to connect to localhost on host machine
-  // For physical device, change to host PC IP address (e.g. 192.168.1.x)
-  static String host = '10.0.2.2';
+  // IP lokal laptop/PC (agar ponsel fisik dalam satu Wi-Fi bisa akses backend)
+  static String host = '192.168.0.106';
 
   static String get authBaseUrl => 'http://$host:8081';
   static String get eventBaseUrl => 'http://$host:8082';
