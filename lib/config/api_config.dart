09f8737a00bdfeae5e090f033829ea0442f385f6
wibisanabama <wibisanabama@gmail.com>
2026-08-20 07:36:11 +0700

    fix(config): support BACKEND_HOST dart-define and standardize gateBaseUrl port to 8086

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,10 +1,9 @@
 class ApiConfig {
-  // Use 10.0.2.2 for Android emulator to connect to localhost on host machine
-  // IP lokal laptop/PC (agar ponsel fisik dalam satu Wi-Fi bisa akses backend)
-  static String host = '192.168.0.106';
+  // Configurable via --dart-define=BACKEND_HOST=... with fallback default
+  static const String host = String.fromEnvironment('BACKEND_HOST', defaultValue: '192.168.0.106');
 
   static String get authBaseUrl => 'http://$host:8081';
   static String get eventBaseUrl => 'http://$host:8082';
   static String get ticketBaseUrl => 'http://$host:8083';
-  static String get gateBaseUrl => 'http://$host:8080';
+  static String get gateBaseUrl => 'http://$host:8086';
 }
