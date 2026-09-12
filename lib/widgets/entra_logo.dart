400803a97c7276c94484b8fb14064de7a110ee4a
wibisanabama <wibisanabama@gmail.com>
2026-09-12 21:49:45 +0700

    feat(ui): display borderless black logo in EntraLogo widget

--- lib/widgets/entra_logo.dart
+++ lib/widgets/entra_logo.dart
@@ -4,45 +4,17 @@ class EntraLogo extends StatelessWidget {
   const EntraLogo({
     super.key,
     this.size = 64,
-    this.backgroundColor = Colors.white,
-    this.borderRadius,
-    this.padding,
-    this.withBorder = true,
   });
 
   final double size;
-  final Color backgroundColor;
-  final BorderRadius? borderRadius;
-  final EdgeInsetsGeometry? padding;
-  final bool withBorder;
 
   @override
   Widget build(BuildContext context) {
-    final effectiveRadius = borderRadius ?? BorderRadius.circular(size * 0.22);
-    final effectivePadding = padding ?? EdgeInsets.all(size * 0.16);
-
-    return Container(
+    return Image.asset(
+      'assets/images/black-logo.png',
       width: size,
       height: size,
-      padding: effectivePadding,
-      decoration: BoxDecoration(
-        color: backgroundColor,
-        borderRadius: effectiveRadius,
-        border: withBorder
-            ? Border.all(color: const Color(0xFFE4E4E7), width: 1.5)
-            : null,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withValues(alpha: 0.04),
-            blurRadius: 10,
-            offset: const Offset(0, 4),
-          ),
-        ],
-      ),
-      child: Image.asset(
-        'assets/images/black-logo.png',
-        fit: BoxFit.contain,
-      ),
+      fit: BoxFit.contain,
     );
   }
 }
